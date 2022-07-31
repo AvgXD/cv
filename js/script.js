8563def7e71de881abb0147923e46f37839f4cbe
Alejandro Vilchis Gil <avg86196@gmail.com>
2022-07-31 13:28:45 -0500

    AVG
    AVG

--- js/script.js
+++ js/script.js
@@ -53,16 +53,6 @@ document.getElementById('form')
 });
 
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/60519614067c2605c0b939c4/1f0vb4gdc';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
 
 $(window).scroll(function() {
     if ($(this).scrollTop() > 300) {
